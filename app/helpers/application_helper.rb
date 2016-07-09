b5f3c0410adc2f5bf8e71602cea2ac174c9b71df
lmqnet <lmqnet@gmail.com>
2016-07-09 15:56:15 +0000

    modify home page and help

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Joey Ruby on Rails Tutorial space"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
