d68ffb4d3d5ec1792b9c00d93da8a86171b6b9b3
lmqnet <lmqnet@gmail.com>
2016-07-13 05:51:49 +0000

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -61,7 +61,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
-  
+
   test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
     assert_no_difference 'User.count' do
