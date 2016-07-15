7a4e09b908fe98f2c20766bf4f3b06d0229bf81a
lmqnet <lmqnet@gmail.com>
2016-07-15 03:32:01 +0000

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get 'sessions/new'
-
-  #get 'users/new'
-
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
@@ -21,10 +13,14 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'sessions/new'
 
-  resources :microposts
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
